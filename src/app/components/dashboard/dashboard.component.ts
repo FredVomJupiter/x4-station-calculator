66eb093fb6f1c7d360e5921a7514fa0cb3a2e828
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-05 15:40:39 +0300

    created a Module interface

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/shared/data.service';
 
+interface Module {
+  name?: string;
+  type?: string;
+  style?: string;
+  input?: object[] | null;
+  output?: object[] | null;
+  amount?: number;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -8,14 +17,17 @@ import { DataService } from 'src/app/shared/data.service';
 })
 export class DashboardComponent implements OnInit {
 
-  data = this.dataService.getData();
+  data = this.dataService.getData().modules;
+
 
   constructor(private dataService: DataService) {
 
   }
 
   ngOnInit(): void {
-   
+    this.data.forEach((module: Module) => {
+      console.log(module);
+    });
   }
 
 
