4480f13c4a29d0af1c0876d3e80fc3205dbd06cf
Frederic Rieg <fredericrieg@protonmail.com>
2023-10-10 18:49:40 +0300

    Making basic tests work with imports & providers

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,15 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DashboardComponent } from './dashboard.component';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DashboardComponent],
+      imports: [
+        MatDialogModule, // You should import MatDialogModule instead of MatDialog
+        NoopAnimationsModule, // This is required to handle animations
+        MatFormFieldModule,
+        MatSelectModule,
+        MatIconModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatTableModule,
+        MatButtonModule,
+        MatInputModule
+      ],
+      providers: [
+        // Mock the MatDialogRef and MAT_DIALOG_DATA
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
+    }).compileComponents(); // Don't forget to compile the components
+
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +44,6 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Add more test cases to validate the functionality of your dialog
 });
--- src/app/dialog/delete/delete.component.spec.ts
+++ src/app/dialog/delete/delete.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteComponent } from './delete.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('DeleteComponent', () => {
   let component: DeleteComponent;
   let fixture: ComponentFixture<DeleteComponent>;
 
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DeleteComponent]
+      declarations: [DeleteComponent],
+      imports: [
+        MatDialogModule, // You should import MatDialogModule instead of MatDialog
+        NoopAnimationsModule, // This is required to handle animations
+        MatFormFieldModule,
+        MatSelectModule,
+        MatIconModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatTableModule,
+        MatButtonModule,
+        MatInputModule
+      ],
+      providers: [
+        // Mock the MatDialogRef and MAT_DIALOG_DATA
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
     });
     fixture = TestBed.createComponent(DeleteComponent);
     component = fixture.componentInstance;
