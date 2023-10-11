fd088a7fdf4d4c3184bde281b0186c3318000a04
Frederic Rieg <fredericrieg@protonmail.com>
2023-10-11 14:47:18 +0300

    feature: collapse for module details

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -5,8 +5,8 @@ interface Module {
   name: string;
   type: string;
   style: string;
-  input: object[] | null;
-  output: object[] | null;
+  input: { name: string, amount: number }[] | null;
+  output: { name: string, amount: number }[] | null;
   amount: number;
 }
 
@@ -26,18 +26,47 @@ export class SummaryComponent {
 
   @Input() list: Module[] = [];
 
+  // This array is used to toggle the details of a module.
+  // If an index is in the array, the respective details are shown.
+  toggle: number[] = [];
+
 
   constructor() { }
 
-
+  /**
+   * Toggles the summary component.
+   */
   toggleSummary() {
     if (this.open == true) {
       this.open = false;
       this.summaryOpen.emit(false);
+      this.toggle = []; // Closing all details when summary is closed.
     } else {
       this.open = true;
       this.summaryOpen.emit(true);
     }
+  }
+
+  /**
+   * Determines whether the details of a module are shown or not.
+   * If the details are shown, the index of the module is added to the toggle array.
+   * @param index The index of the module in the list.
+   */
+  toggleDetails(index: number) {
+    if (this.toggle.includes(index)) {
+      this.toggle.splice(this.toggle.indexOf(index), 1);
+    } else {
+      this.toggle.push(index);
+    }
+  }
+
+  /**
+   * 
+   * @param index The index of the module in the list.
+   * @returns true if the details of the module are shown, false if not.
+   */
+  check(index: number) {
+    return this.toggle.includes(index);
   }
 
 }
